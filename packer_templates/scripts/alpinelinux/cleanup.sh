099b813d8ad31ba18ad7235f0a7b458297d26477
Naomi Rennie-Waldock <naomi.renniewaldock@gmail.com>
2024-12-31 05:21:33 +0000

    Add Alpine Linux 3.21
    Signed-off-by: Naomi Rennie-Waldock <naomi.renniewaldock@gmail.com>

---
+++ packer_templates/scripts/alpinelinux/cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -eux
+
+echo "Remove development and kernel source packages"
+apk del alpine-sdk
+
+echo "Removing extra firmware packages"
+apk add --no-cache linux-firmware-none
+
+echo "clean all package cache information"
+rm -rf /var/cache/apk/*
+
+echo "truncate any logs that have built up during the install"
+find /var/log -type f -exec truncate --size=0 {} \;
+
+echo "remove the contents of /tmp and /var/tmp"
+rm -rf /tmp/* /var/tmp/*
+
+echo "Clear the history so our install commands aren't there"
+rm -f /root/.wget-hsts
+export HISTSIZE=0
